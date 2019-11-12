67f60ed3a91aa2df6807f4bd38bfbe80a4b7e35a
Bastien <bastien_maurice@hotmail.com>
2019-11-12 22:31:51 +0100

    ajout header et footer et mat icons offline

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const { app, BrowserWindow } = require('electron')
 
+
+let win;
+let isToolsDev;
+const args = process.argv.slice(1);
+isToolsDev = args.some(val => val === '--devTools');
+
+
 function createWindow () {
-  let win = new BrowserWindow({
+    let win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+        nodeIntegration: true
     },
     frame: false,
-    titleBarStyle: 'hiden'
+    titleBarStyle: 'hidden',
+    icon: './src/assets/icon/icon_transparent.png',
+    show: false
+    })
 
-  })
+    require('electron-reload')(__dirname, {
+        electron: require(`${__dirname}/node_modules/electron`),
+        hardResetMethod: 'exit',
+        argv: ['--devTools']
+    });
 
-  win.loadURL('http://localhost:4200/')
+
+    win.once('ready-to-show', () => {
+        win.show();
+    });
+
+
+    win.loadURL('http://localhost:4200/');
+
+    if (isToolsDev) {
+        win.webContents.openDevTools();
+    }
 }
 
 app.on('ready', createWindow)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule} from '@angular/material'
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {HeaderComponent} from './core/header/header.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {NgxElectronModule} from 'ngx-electron';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatMenuModule,
+        FlexLayoutModule,
+        MatIconModule,
+        MatButtonModule,
+        NgxElectronModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
