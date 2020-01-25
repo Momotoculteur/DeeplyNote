900098db4fa96ad36b260c1e5efaada982f2179f
Bastien <bastien_maurice@hotmail.com>
2020-01-25 16:57:37 +0100

    suppression karma et protractor

--- main.js
+++ main.js
@@ -1,113 +1,97 @@
-const { app, BrowserWindow, dialog, ipcMain } = require('electron')
-const fs = require('fs')
-const Store = require('electron-store');
-const path =  require('path');
-const url = require('url');
-const storage = new Store({name:'settings'});
-
-let win;
-let isToolsDev;
-const args = process.argv.slice(1);
-isToolsDev = args.some(val => val === '--devTools');
-
-
-function createWindow () {
+"use strict";
+exports.__esModule = true;
+var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow, dialog = _a.dialog, ipcMain = _a.ipcMain;
+var fs = require("fs");
+var Store = require("electron-store");
+var path = require("path");
+var url = require("url");
+var storage = new Store({ name: 'settings' });
+var win;
+var isToolsDev;
+var args = process.argv.slice(1);
+isToolsDev = args.some(function (val) { return val === '--devTools'; });
+function createWindow() {
     win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-        nodeIntegration: true
-    },
-    frame: false,
-    titleBarStyle: 'hidden',
-    icon: './src/assets/icon/icon_transparent.png',
-    show: false
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            allowRunningInsecureContent: (isToolsDev) ? true : false
+        },
+        frame: false,
+        titleBarStyle: 'hidden',
+        icon: './src/assets/icon/icon_transparent.png',
+        show: false
     });
-
     initSettingsPreferences();
-
-
     if (isToolsDev) {
         require('electron-reload')(__dirname, {
-            electron: require(`${__dirname}/node_modules/electron`),
+            electron: require(__dirname + "/node_modules/electron"),
             hardResetMethod: 'exit',
             argv: ['--devTools']
         });
         win.webContents.openDevTools();
         win.loadURL('http://localhost:4200/');
-    } else {
+    }
+    else {
         //win.webContents.openDevTools();
-
         win.loadURL(url.format({
             pathname: path.join(__dirname, 'dist/DeeplyNote/index.html'),
             protocol: 'file:',
             slashes: true
         }));
         //console.log(path.join(__dirname, 'dist/DeeplyNote/index.html'))
-
     }
-
-
-    win.once('ready-to-show', () => {
+    win.once('ready-to-show', function () {
         win.show();
     });
 }
-
-app.on('ready', createWindow)
-
-
-ipcMain.on('pingOpenFolderDirectory', (event, message) => {
-    const options = {
+app.on('ready', createWindow);
+ipcMain.on('pingOpenFolderDirectory', function (event, message) {
+    var options = {
         title: 'Ouvrir un dossier',
-        properties: ['openDirectory'],
+        properties: ['openDirectory']
     };
-    dialog.showOpenDialog(win, options
-    ).then(result => {
-
-        if(!result.canceled) {
-            const listFiles = fs.readdirSync(result.filePaths[0]);
-            let filesList = listFiles.filter( function( elm ) {return elm.match(/.*\.(txt)/ig);});
-            const response = {
+    dialog.showOpenDialog(win, options).then(function (result) {
+        if (!result.canceled) {
+            var listFiles = fs.readdirSync(result.filePaths[0]);
+            var filesList = listFiles.filter(function (elm) { return elm.match(/.*\.(txt)/ig); });
+            var response = {
                 path: result.filePaths[0],
                 files: filesList
             };
             event.reply('responseOpenFolderDirectory', response);
-
-        } else {
-            console.log('Annulation par l utilisateur')
         }
-    }).catch(err => {
+        else {
+            console.log('Annulation par l utilisateur');
+        }
+    })["catch"](function (err) {
         console.log(err);
-    })
+    });
 });
-
-ipcMain.on('pingDisplayFile', (event, message) => {
-    const fileContent = fs.readFileSync(message, 'utf8');
-    const lineNumber = fileContent.split('\n').length;
-    const response = {
+ipcMain.on('pingDisplayFile', function (event, message) {
+    var fileContent = fs.readFileSync(message, 'utf8');
+    var lineNumber = fileContent.split('\n').length;
+    var response = {
         file: fileContent,
         line: lineNumber
     };
     event.reply('responseFileContent', response);
 });
-
-ipcMain.on( 'saveFile', (event, data) => {
+ipcMain.on('saveFile', function (event, data) {
     fs.writeFileSync(data.file.path, data.content);
 });
-
-ipcMain.on('loadUserSettings', (event, data) => {
+ipcMain.on('loadUserSettings', function (event, data) {
     event.reply('responseLoadUserSettings', storage.get('SETTINGS'));
 });
-
-ipcMain.on('saveSettings' , (event, data) => {
+ipcMain.on('saveSettings', function (event, data) {
     storage.set('SETTINGS.THEME_TYPE', data.type);
     storage.set('SETTINGS.CUSTO_PALETTE', data.props);
 });
-
-function initSettingsPreferences()
-{
-    if(storage.get('SETTINGS')) {
-    } else {
+function initSettingsPreferences() {
+    if (storage.get('SETTINGS')) {
+    }
+    else {
         storage.set({
             'SETTINGS': {
                 'THEME_TYPE': 'DARK',
@@ -117,13 +101,10 @@ function initSettingsPreferences()
                     '--backgroundColor-ExplorerBar': '#333333',
                     '--textColor-Highlight': '#333333',
                     '--backgroundColor-LineContent': '#333333',
-
                     '--textColor-MainBar': '#c8c8c8',
                     '--textColor-Editor': '#333333'
                 }
             }
-
         });
-
     }
 }
